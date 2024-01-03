c38c38da5339e6b4e343b7b4ebb1a4cd5c31d685
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-03 21:02:48 +0100

    container component created

---
+++ src/components/Container/container.tsx
@@ -0,0 +1,7 @@
+export const Container = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className='my-0 mx-auto text-left p-[20px] max-w-3xl w-full leading-normal'>
+      {children}
+    </main>
+  );
+};
