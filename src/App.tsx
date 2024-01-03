e00aa22dbbc794bf0833584b2591185b911c2baf
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-03 21:03:31 +0100

    app.tsx udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import { CSVReader } from './excise-tax-calculator/CSVReader/csvReader';
+import { Container } from './components/Container/container';
+import { Header } from './components/Header/header';
 
+function App() {
   return (
-    <></>
-  )
+    <Container>
+      <Header title='Beer excise tax calculator' />
+      <CSVReader />
+    </Container>
+  );
 }
 
-export default App
+export default App;
