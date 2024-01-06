f0dead82a03f3a3ff4227c70c06c3ad8ea883a6b
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-06 17:04:52 +0100

    resizer component created

---
+++ src/components/Resizer/resizer.tsx
@@ -0,0 +1,31 @@
+import { Header, Table } from '@tanstack/react-table';
+import { Beer, RowData } from '../../excise-tax-calculator/Table/table';
+
+interface ResizerProps {
+  header: Header<RowData | Beer, unknown>;
+  table: Table<RowData | Beer>;
+}
+
+export const Resizer = ({ header, table }: ResizerProps) => {
+  return (
+    <div
+      {...{
+        onDoubleClick: () => header.column.resetSize(),
+        onMouseDown: header.getResizeHandler(),
+        onTouchStart: header.getResizeHandler(),
+        className: `absolute right-0 top-0 h-[25px] w-[5px] select-none bg-black opacity-0 hover:cursor-col-resize hover:touch-none hover:opacity-100 ${
+          table.options.columnResizeDirection
+        } ${header.column.getIsResizing() ? 'opacity-100 bg-blue-700' : ''}`,
+        style: {
+          transform:
+            table.options.columnResizeMode === 'onEnd' &&
+            header.column.getIsResizing()
+              ? `translateX(${
+                  1 * (table.getState().columnSizingInfo.deltaOffset ?? 0)
+                }px)`
+              : '',
+        },
+      }}
+    />
+  );
+};
