50d85d9df4615488766fd1821b2cc86a2df1594a
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-07 16:33:33 +0100

    store added, types moved

--- src/excise-tax-calculator/CSVReader/csvReader.tsx
+++ src/excise-tax-calculator/CSVReader/csvReader.tsx
@@ -1,32 +1,24 @@
-import { HTMLAttributes, useState } from 'react';
 import { useCSVReader } from 'react-papaparse';
-import { Table } from '../Table/table';
-
-interface ParsedResults {
-  data: RowData[];
-}
-
-interface RowData {
-  [key: number]: string | undefined;
-}
-
-interface ParserProps {
-  getRootProps: () => HTMLAttributes<HTMLElement>;
-  acceptedFile: File;
-  getRemoveFileProps: () => HTMLAttributes<HTMLElement>;
-}
+import useCalculatorStore from '../../utils/calculatorStore';
+import {
+  ParsedResults,
+  ParserProps,
+} from '../../components/api/types/csvReaderTypes';
 
 export const CSVReader = () => {
   const { CSVReader } = useCSVReader();
-  const [data, setData] = useState<RowData[]>([]);
+  const { setData, removeData, setAcceptedFileName } = useCalculatorStore();
   const platoRegex = /\d\d,\d° | \d\d,\d\d° | \d,\d°/;
   const volumeRegex =
     /\s(0,5 l|0,75 l|0,44 l|0,33 l|0,375 l| 0,750 l |30 l|20 l|10 l)/g;
   const packageTypeRegex = /but\.|but|can|keg|keykeg/;
 
+  console.log();
+
   return (
     <CSVReader
-      onUploadAccepted={(results: ParsedResults) => {
+      onUploadAccepted={(results: ParsedResults, file: File) => {
+        setAcceptedFileName(file.name);
         const parsedData = results.data
           .map((row) => {
             const orderNumber = row[0];
@@ -73,12 +65,15 @@ export const CSVReader = () => {
             </div>
             <button
               {...getRemoveFileProps()}
+              onClick={(event) => {
+                removeData();
+                getRemoveFileProps().onClick?.(event);
+              }}
               className='mx-0 my-[5px] h-[65px] cursor-pointer bg-[#A01919] px-[25px] py-[10px] font-medium leading-6 text-white outline-none hover:bg-[#DD2222] md:h-[44px]'
             >
               Remove
             </button>
           </div>
-          <Table data={data} />
         </>
       )}
     </CSVReader>
