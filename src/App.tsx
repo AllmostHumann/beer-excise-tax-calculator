60f6324bcb9fa7d69daa61095361bb8b5e34b1ac
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-07 16:34:13 +0100

    ExciseTaxCalculator component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { CSVReader } from './excise-tax-calculator/CSVReader/csvReader';
 import { Container } from './components/Container/container';
 import { Header } from './components/Header/header';
+import { ExciseTaxCalculator } from './excise-tax-calculator/exciseTaxCalculator';
 
 function App() {
   return (
     <Container>
       <Header title='Beer excise tax calculator' />
-      <CSVReader />
+      <ExciseTaxCalculator />
     </Container>
   );
 }
