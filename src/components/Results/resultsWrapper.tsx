492e8b2750b91e44d8b62c489a3f4bb5bacfeac3
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 22:57:26 +0100

    key prop corrected

--- src/components/Results/resultsWrapper.tsx
+++ src/components/Results/resultsWrapper.tsx
@@ -1,14 +1,10 @@
 interface ResultsWrapperProps {
   children: React.ReactNode;
-  key: string | undefined;
 }
 
-export const ResultsWrapper = ({ key, children }: ResultsWrapperProps) => {
+export const ResultsWrapper = ({ children }: ResultsWrapperProps) => {
   return (
-    <div
-      key={key}
-      className='flex flex-col rounded-md border-[1px] border-solid border-sherpaBlue'
-    >
+    <div className='flex flex-col rounded-md border-[1px] border-solid border-sherpaBlue'>
       {children}
     </div>
   );
