0c052a3f9e060fd9806e7182aa259496be116462
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 22:52:17 +0100

    calculated Results component refactored

--- src/excise-tax-calculator/CalculatedResults/calculatedResults.tsx
+++ src/excise-tax-calculator/CalculatedResults/calculatedResults.tsx
@@ -1,82 +1,45 @@
-import WarningIcon from './icons/warning.svg?react';
 import useCalculatorStore from '../../utils/calculatorStore';
 import { calculatedPlatoValues } from './unitResults';
+import { ResultField } from '../../components/Results/resultsField';
+import { ResultText } from '../../components/Results/resultsText';
+import { ResultsWrapper } from '../../components/Results/resultsWrapper';
+import { ResultsContainer } from '../../components/Results/resultsContainer';
+import { DiscountButton } from '../../components/Buttons/DiscountButton/discountButton';
+import { WarningText } from '../../components/WarningText/warningText';
+import { ResultsTitle } from '../../components/Results/resultsTitle';
 
 export const CalculatedResults = () => {
-  const { data, acceptedFileName, discount, setDiscount } =
-    useCalculatorStore();
-
-  const useDiscountForSmallProducers = () => {
-    setDiscount(discount === 1 ? 2 : 1);
-  };
+  const { data, discount } = useCalculatorStore();
 
   const resultsForPlato = calculatedPlatoValues(data, discount);
 
   return (
     <div>
-      <div className='mb-4 flex flex-row items-center justify-center font-bold'>
-        <WarningIcon className='md:w6 w-12 pr-2' />
-        <p>
-          Tax for beer in 2024 - 10.40 zł per hectolitre for each degree Plato
-          of the finished product.
-        </p>
-      </div>
-      <div className={`m-4 ${acceptedFileName ? 'block' : 'hidden'}`}>
-        <label
-          className='px-1 pt-1 font-bold'
-          htmlFor='discount'
-        >
-          Discount for small producers (50%)
-        </label>
-        <input
-          id='discount'
-          onClick={useDiscountForSmallProducers}
-          className='translate-y-0.5'
-          type='checkbox'
-          name='discountCheckBox'
-          defaultChecked={discount === 2 ? true : false}
-        />
-      </div>
-
-      <div className='grid grid-cols-[1fr_1fr] gap-1 md:mx-5 md:grid-cols-[1fr_1fr_1fr_1fr_1fr]'>
+      <WarningText />
+      <DiscountButton />
+      <ResultsContainer>
         {resultsForPlato.map((result) => (
-          <div
-            key={result.plato}
-            className='flex flex-col border-[2px] border-solid p-1'
-          >
-            <div className='flex flex-row font-normal md:font-medium'>
-              Extract:
-              <p className='pl-1 font-semibold md:font-bold'>
-                {result.plato} Plato
-              </p>
-            </div>
-            <div className='flex flex-row font-normal md:font-medium'>
+          <ResultsWrapper key={result.plato}>
+            <ResultsTitle result={result.plato} />
+            <ResultField>
               {window.innerWidth < 1024 ? 'U.' : 'Units'} volume:
-              <p className='pl-1 font-semibold md:font-bold'>
-                {result.unitPackagesSumQuantities} hl
-              </p>
-            </div>
-            <div className='flex flex-row font-normal md:font-medium'>
+              <ResultText>{result.unitPackagesSumQuantities} hl</ResultText>
+            </ResultField>
+            <ResultField>
               {window.innerWidth < 1024 ? 'U.' : 'Units'} tax:
-              <p className='pl-1 font-semibold md:font-bold'>
-                {result.unitTax} zł
-              </p>
-            </div>
-            <div className='flex flex-row font-normal md:font-medium'>
+              <ResultText>{result.unitTax} zł</ResultText>
+            </ResultField>
+            <ResultField>
               {window.innerWidth < 1024 ? 'B.' : 'Barrels'} volume:
-              <p className='pl-1 font-semibold md:font-bold'>
-                {result.barrelsSumQuantities} hl
-              </p>
-            </div>
-            <div className='flex flex-row font-normal md:font-medium'>
+              <ResultText>{result.barrelsSumQuantities} hl</ResultText>
+            </ResultField>
+            <ResultField>
               {window.innerWidth < 1024 ? 'B.' : 'Barrels'} tax:
-              <p className='pl-1 font-semibold md:font-bold'>
-                {result.barrelsTax} zł
-              </p>
-            </div>
-          </div>
+              <ResultText> {result.barrelsTax} zł</ResultText>
+            </ResultField>
+          </ResultsWrapper>
         ))}
-      </div>
+      </ResultsContainer>
     </div>
   );
 };
