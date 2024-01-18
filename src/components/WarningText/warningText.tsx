daefcbf3b351ff8c4c6d7aa627c8fb414e492bbb
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 22:50:45 +0100

    WarningText component created

---
+++ src/components/WarningText/warningText.tsx
@@ -0,0 +1,13 @@
+import WarningIcon from './icons/warning.svg?react';
+
+export const WarningText = () => {
+  return (
+    <div className='mb-4 flex flex-row items-center justify-center font-bold'>
+      <WarningIcon className='md:w6 w-12 pr-2' />
+      <p className='text-[14px] font-medium text-white md:text-[16px] md:font-semibold'>
+        Tax for beer in 2024 - 10.40 zł per hectolitre for each degree Plato of
+        the finished product.
+      </p>
+    </div>
+  );
+};
