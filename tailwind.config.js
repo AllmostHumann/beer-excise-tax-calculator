4f31111a39afed0719f20a26d76c35e737b10442
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 22:47:37 +0100

    custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
       },
+      colors: {
+        alto: '#D9D9D9',
+        silverChalice: '#B1AFAF',
+        silver: '#CCCCCC',
+        japaneseLaurel: '#008000',
+        limeade: '#3AB701',
+        sunsetOrange: '#FF4747',
+        sherpaBlue: '#004C4C',
+        jet: '#333333',
+      },
     },
   },
   plugins: ['prettier-plugin-tailwindcss'],
