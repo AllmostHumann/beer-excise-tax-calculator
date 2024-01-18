49883b22661f7049ee7898c3741f0ed09d9db610
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 22:51:01 +0100

    discount button component created

---
+++ src/components/Buttons/DiscountButton/discountButton.tsx
@@ -0,0 +1,27 @@
+import useCalculatorStore from '../../../utils/calculatorStore';
+
+export const DiscountButton = () => {
+  const { acceptedFileName, discount, setDiscount } = useCalculatorStore();
+  const useDiscountForSmallProducers = () => {
+    setDiscount(discount === 1 ? 2 : 1);
+  };
+
+  return (
+    <div className={`m-4 ${acceptedFileName ? 'block' : 'hidden'}`}>
+      <label
+        className='px-1 pt-1 text-[14px] font-medium text-white md:text-[16px] md:font-semibold'
+        htmlFor='discount'
+      >
+        Discount for small producers (50%)
+      </label>
+      <input
+        id='discount'
+        onClick={useDiscountForSmallProducers}
+        className='size-4 translate-y-0.5 hover:fill-red-400 md:hover:scale-[1.15]'
+        type='checkbox'
+        name='discountCheckBox'
+        defaultChecked={discount === 2 ? true : false}
+      />
+    </div>
+  );
+};
