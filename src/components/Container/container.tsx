e242ce69e6d5265d76a6b0c8347e692778367b3b
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-18 18:58:19 +0100

    styles update

--- src/components/Container/container.tsx
+++ src/components/Container/container.tsx
@@ -1,6 +1,6 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='mx-auto my-0 w-full max-w-6xl px-4 py-2 text-left leading-normal md:p-5'>
+    <main className='mx-auto my-0 w-full max-w-6xl px-4 py-1 text-left leading-normal md:p-5'>
       {children}
     </main>
   );
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,7 @@
 export const Header = ({ title }: { title: string }) => {
   return (
     <header>
-      <h1 className='m-0 grid py-[10px] text-[30px] font-bold md:text-[30px]'>
+      <h1 className='m-0 grid place-items-center py-[10px] text-[30px] font-bold md:text-[30px]'>
         {title}
       </h1>
     </header>
