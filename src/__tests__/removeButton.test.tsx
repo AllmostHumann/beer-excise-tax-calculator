d8a4222167b6e0bba95f62a76e48c0787d1aae6b
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-23 21:06:51 +0100

    component name changed

--- src/__tests__/removeButton.test.tsx
+++ src/__tests__/removeButton.test.tsx
@@ -1,15 +1,15 @@
-import { BrowseRemoveButton } from '../components/Buttons/ReaderButtons/RemoveButton';
+import { RemoveButton } from '../components/Buttons/ReaderButtons/RemoveButton';
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
 test('Renders the remove button', () => {
-  render(<BrowseRemoveButton />);
+  render(<RemoveButton />);
   expect(true).toBeTruthy();
 });
 
 test('Test remove button click event,', () => {
-  render(<BrowseRemoveButton />);
+  render(<RemoveButton />);
   const removeButton = screen.getByTestId('removeButton');
   userEvent.click(removeButton);
 });
