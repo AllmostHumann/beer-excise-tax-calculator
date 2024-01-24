ba72ec18ae4762e8709108678ecce68199d9ae72
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-01-24 17:47:12 +0100

    table render test added

--- src/__tests__/table.test.tsx
+++ src/__tests__/table.test.tsx
@@ -1,8 +1,8 @@
-import { Table } from '../excise-tax-calculator/Table/Table';
+import { Table as TableComponent } from '../excise-tax-calculator/Table/Table';
 import { expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 
 test('Renders the table', () => {
-  render(<Table />);
+  render(<TableComponent />);
   expect(true).toBeTruthy();
 });
